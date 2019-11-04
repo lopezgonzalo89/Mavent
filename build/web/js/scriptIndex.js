85df671ff294307aadfac71cdbe2b651c181d3c6
lopezgonzalo89@ <lopezgonzalo.sm89@gmail.com>
2019-11-04 06:07:14 -0300

    Save

--- build/web/js/scriptIndex.js
+++ build/web/js/scriptIndex.js
@@ -1,18 +1,34 @@
 $(document).ready(function () {
-    $("#formData").on("submit", function (e) {        
-        e.preventDefault();        
+    $("#formData").on("submit", function (e) {
+        e.preventDefault();
+        // Validar nombre
+        nombre = document.getElementById("nombre").value;
+        if (nombre.length === 0) {
+            document.getElementById("llenarNombre").innerHTML="Ingrese un Nombre";
+            return false;
+        } else if (nombre.length > 100) {
+            document.getElementById("llenarNombre").innerHTML="El nombre no puede superar los 100 caracteres";
+            return false;
+        } else {
+            document.getElementById("llenarNombre").innerHTML="";
+        }
+        // Validar monto
+        monto = document.getElementById("monto").value;        
+        if (monto.length === 0) {
+            document.getElementById("llenarMonto").innerHTML="Ingrese un Monto";
+            return false;
+        } else {
+            document.getElementById("llenarMonto").innerHTML="";
+        }
         $.ajax({
             type: $(this).attr("method"),
             url: $(this).attr("action"),
             data: $(this).serialize(),
 
-            success: function (resp) {
-                console.log(resp);
-                $("#respuesta").html("Pedido cargado correctamente <br> Detalle de prueba: <br>" + resp);
+            success: function () {
+                $("#respuesta").html("Pedido cargado correctamente");                
             },
-            error: function (jqXHR, estado) {
-                console.log(jqXHR);
-                console.log("Estado: " + estado);
+            error: function () {
                 $(".respuesta").html("Fallo al cargar pedido");
             }
         });
